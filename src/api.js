61443d657155ba1ca0558dee4455200ee5685661
Bar Gavriely <bar.gavriely@gmail.com>
2024-07-16 18:21:28 +0300

    added backend url

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
-    withCredentials: true,
+  baseURL:
+    "https://railway.app/project/5365015c-d103-4bcd-ab15-5cc59cc49abd/service/eeaf78bd-1e93-442a-b97c-292b29f393dc0",
+  withCredentials: true,
 });
 
 export default api;
