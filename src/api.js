4942ecc9dc2ec0a55fb3b446e574579378cebba9
Bar Gavriely <bar.gavriely@gmail.com>
2024-07-19 16:05:56 +0300

    missed semi colon

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https:/resumebackend-production.up.railway.app",
+  baseURL: "https://resumebackend-production.up.railway.app",
   withCredentials: true,
 });
 
