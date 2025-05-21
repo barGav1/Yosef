dd60baa7939490b34aee5260e6d484152261a0b1
bargav1 <bar.gavriely@gmail.com>
2025-05-21 13:58:48 +0300

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/Default.js",
-    "./src/pages/forgotPass.js",
-    "./src/pages/ResumeMaker.js",
-    "./src/pages/ResumeViewer.js",
-    "./src/pages/Register.js",
-    "./src/pages/Login.js",
-    "./src/components/Navbar.js",
-    "./src/components/Header.js",
-    "./src/components/Card.js",
-    "./src/components/Buttons.js",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {},
